a18b354f491262a80c4f35e1732608c34f0c57a5
Rafael Queiroz <rafaelqueiroz@seatelecom.com.br>
2026-05-10 01:41:07 -0300

    mensagem

--- server.js
+++ server.js
@@ -7,19 +7,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Banco falso em memória
 let users = [];
 let pets = [];
 let agendamentos = [];
 
-// ROTA TESTE
 app.get("/", (req, res) => {
   res.send("API Petshop rodando 🐶");
 });
-
-// =======================
-// LOGIN / REGISTRO
-// =======================
 
 app.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
@@ -92,9 +86,67 @@ app.post("/login", async (req, res) => {
       email: user.email,
     },
   });
-  const PORT = process.env.PORT || 3001;
+});
+
+app.get("/pets", (req, res) => {
+  res.json(pets);
+});
+
+app.post("/pets", (req, res) => {
+  const { nome, especie, raca, idade } = req.body;
+
+  if (!nome || !especie) {
+    return res.status(400).json({
+      message: "Nome e espécie do pet são obrigatórios.",
+    });
+  }
+
+  const pet = {
+    id: pets.length + 1,
+    nome,
+    especie,
+    raca,
+    idade,
+  };
+
+  pets.push(pet);
+
+  res.status(201).json({
+    message: "Pet cadastrado com sucesso!",
+    pet,
+  });
+});
+
+app.get("/agendamentos", (req, res) => {
+  res.json(agendamentos);
+});
+
+app.post("/agendamentos", (req, res) => {
+  const { nomePet, data, servico } = req.body;
+
+  if (!nomePet || !data || !servico) {
+    return res.status(400).json({
+      message: "Nome do pet, data e serviço são obrigatórios.",
+    });
+  }
+
+  const agendamento = {
+    id: agendamentos.length + 1,
+    nomePet,
+    data,
+    servico,
+  };
+
+  agendamentos.push(agendamento);
+
+  res.status(201).json({
+    message: "Agendamento realizado com sucesso!",
+    agendamento,
+  });
+});
+
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log('Servidor rodando na porta ${PORT}');
-});
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
